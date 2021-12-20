23b44d34f9327a1b4666f13a9caa58f9ced0d3bc
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-19 22:50:11 -0300

    feat: add global variables of styles

---
+++ src/theme/global.js
@@ -0,0 +1,29 @@
+export const global = {
+  color: {
+    primary: 'var(--color-primary)',
+    secondary: 'var(--color-secondary)',
+    gray: {
+      1: 'var(--gray-1)',
+      2: 'var(--gray-2)',
+      3: 'var(--gray-3)',
+    },
+  },
+  p: {
+    1: 'var(--p-1)',
+    2: 'var(--p-2)',
+    3: 'var(--p-3)',
+  },
+  m: {
+    1: 'var(--m-1)',
+    2: 'var(--m-2)',
+    3: 'var(--m-3)',
+  },
+  r: {
+    1: 'var(--r-1)',
+    2: 'var(--r-2)',
+    3: 'var(--r-3)',
+  },
+  container: {
+    '2xl': '1536px',
+  },
+}
