cf621bae15ecf4360c76e836dff77a080b228e76
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-19 22:49:14 -0300

    feat: add src like a root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { readdirSync } from 'fs'
+
+const absolutePathAliases = {}
+
+// Root resources folder
+const srcPath = path.resolve('./src/')
+
+// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map(
+  (dirent) => dirent.name.replace(/(\.ts){1}(x?)/, '')
+)
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory)
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      ...absolutePathAliases,
+    },
+  },
 })
