ead4f88fced888e70aa40d389dc1670579d291a9
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-22 15:25:57 -0300

    feat(web): get categories from api

---
+++ web/src/services/getCategories.js
@@ -0,0 +1,26 @@
+import { generateQuery } from './utils/generateQuery'
+
+export const getCategories = async () => {
+  const query = generateQuery(['subcategories', 'subcategories.icon'])
+
+  const categories = await fetch(
+    `http://localhost:1337/api/categories?${query}`
+  )
+    .then((res) => res.json())
+    .then(({ data }) =>
+      data.map((category) => ({
+        id: category.id,
+        ...category.attributes,
+        subcategories: getSubCategories(category),
+      }))
+    )
+
+  return categories
+}
+
+const getSubCategories = (category) => {
+  return category.attributes.subcategories.data.map((subcategory) => ({
+    id: subcategory.id,
+    ...subcategory.attributes,
+  }))
+}
