ab20b3af6555e21bba6abf84a5e8e4cd5f082b71
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-23 22:57:37 -0300

    feat(web): style margins in homepage

---
+++ web/src/theme/app-styles.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { global } from './global'
+
+export const Container = styled.section`
+  max-width: ${global.container['2xl']};
+  margin-left: auto;
+  margin-right: auto;
+`
