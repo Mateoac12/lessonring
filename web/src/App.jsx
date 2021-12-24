4be66d81ef8dc12a324b2a7cf5ff8a59d6eb5e79
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-23 22:56:40 -0300

    feat(web): add horizontal margins in all page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Switch } from "wouter"
 import HomePage from "pages/Home"
 import Header from "components/layout/Header"
+import { Container } from "theme/app-styles"
 
 function App() {
   return <>
     <Header />
-    <Switch>
-      <Route path="/" component={HomePage} />
-    </Switch>
+    <Container>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Container>
   </>
 }
 
