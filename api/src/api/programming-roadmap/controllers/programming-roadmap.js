6b052a4cb1ff0ea494b58667c0fdba40be448fca
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-23 22:54:47 -0300

    feat(api): add courses and roadmap

---
+++ api/src/api/programming-roadmap/controllers/programming-roadmap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  programming-roadmap controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::programming-roadmap.programming-roadmap');
---
+++ api/src/api/programming-roadmap/routes/programming-roadmap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * programming-roadmap router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::programming-roadmap.programming-roadmap');
