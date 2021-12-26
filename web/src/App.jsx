45c89ab71bbdb3bd04399118f180b4d23207baf4
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-25 22:03:34 -0300

    feat(web): add podcast route and footer

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Switch } from "wouter"
 import HomePage from "pages/Home"
 import Header from "components/layout/Header"
 import { Container } from "theme/app-styles"
+import Footer from "components/layout/Footer"
+import PodcastPage from "pages/Podcast"
 
 function App() {
   return <>
@@ -9,8 +11,10 @@ function App() {
     <Container>
       <Switch>
         <Route path="/" component={HomePage} />
+        <Route path="/podcasts" component={PodcastPage} />
       </Switch>
     </Container>
+    <Footer />
   </>
 }
 
