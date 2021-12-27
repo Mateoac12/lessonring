439dcb79d3260395eccbb42b738e93c8491c8ed5
Mateo Álvarez <alvarez.fing@gmail.com>
2021-12-26 21:17:11 -0300

    feat(web): apply list of podcasts

--- web/src/pages/Podcast/index.jsx
+++ web/src/pages/Podcast/index.jsx
@@ -1,8 +1,17 @@
 import MainTitle from "components/atoms/MainTitle"
+import ArticleList from "components/molecules/ArticleList"
+import { usePodcasts } from "hook/usePodcasts"
 
 const PodcastPage = () => {
+  const { podcasts, isLoading } = usePodcasts()
   return <>
     <MainTitle>Poscast</MainTitle>
+
+    {
+      isLoading
+        ? <p>Loading...</p>
+        : <ArticleList podcasts={podcasts} />
+    }
   </>
 }
 
